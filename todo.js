1bdf6f901eb1ffd54985a799305ece8495ad08e5
Eman-El-Zetawy <dovprogrammer@gmail.com>
2019-10-06 11:37:28 -0700

    update javascript

--- todo.js
+++ todo.js
@@ -1,25 +1,68 @@
-var input = document.getElementById("input");
-var outbox = document.getElementById("outbox");
-var inbox = document.getElementById("inbox");
-var iconx = document.getElementById("iconX");
-var li = document.createElement("li");
-var test =document.getElementById("test");
-var radio = document.getElementById("radio");
-var inra =document.getElementById("inra");
 
-   input.addEventListener("click" , function  () {
-   iconx.innerHTML= '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>' ;                        });
-   iconx.addEventListener("click" ,function  (){
-       // test.innerHTML=" ";
-   test.innerHTML+='<li><input type="radio" id="inra" ><span id="radio"></span> </li>'  ;               });
-//    inra.addEventListener("click" , function (){
-//        var inputValue =input.value;
-//        console.log(inputValue) ;
-//         radio.innerHTML= input.value ;
-//    });
-   // if(event.which===13){
-    //     console.log("ghafbd");
-    // }
-    // test.appendChild(li);
-           // li.value="fjklhk";
-    // input.appendChild( document.createTextNode(input));
+var input = document.getElementById("input");
+var iconx = document.getElementById("iconX");
+var ulli = document.getElementById("ulli");
+var arr=[];
+
+
+
+input.addEventListener("input" , function  () {
+iconx.innerHTML= '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>' ;                       
+  });
+
+
+ input.addEventListener("click" , function  () {
+    input.value="";});
+
+
+ iconx.addEventListener("click" ,function  (){
+
+    if(input.value!==""){
+        arr.push(input.value);
+        var li = document.createElement('LI');
+        
+        
+        // var svg =document.createTextNode('<svg id="s0" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/> <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>');
+
+        var inputValue = document.createTextNode(input.value);
+        
+        li.appendChild(inputValue); 
+        li.setAttribute("id", "test");
+
+        // ulli.appendChild(li);
+        console.log(inputValue);
+        console.log(arr.length);
+
+        var aul = document.getElementById("au1");
+        ulli.innerHTML+= '<li id="test" ><svg id="s0" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>&nbsp<span id="span" >'+ input.value +'</span><svg id="sx" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path opacity=".87" fill="none" d="M0 0h24v24H0V0z"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z"/></svg>&nbsp</li>';
+        var so = document.getElementById("s0");
+        var span = document.getElementById("span");
+        var sx = document.getElementById("sx");
+       var test =document.getElementById("test");
+        so.addEventListener("click" , function (){
+            so.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4 8-8z"/></svg';
+             span.style= "text-decoration-line :line-through ";
+           });
+           sx.addEventListener("click" , function (){
+               test.innerHTML= ""; 
+           })
+          
+      
+      // ulli.innerHTML+= '<li id="" ><svg id="s0" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>&nbsp<span id="span" >'+ input.value +'</span><svg id="sx" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path opacity=".87" fill="none" d="M0 0h24v24H0V0z"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z"/></svg>&nbsp</li>';
+      aul.addEventListener("click" , function(){
+        so.innerHTML= '<svg id="s0" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>' ;
+        span.style = "text-decoration-line:none" ; 
+   
+       }) 
+
+
+      var so = document.getElementById("s0");
+        var span = document.getElementById("span");
+        var sx = document.getElementById("sx");
+       var test =document.getElementById("test");
+
+
+
+    }
+
+});
